0a0471e75c050c6a16f86746a942da1ad05d4448
markdias <mark@mdias.co.uk>
2024-05-28 09:58:43 +0100

    Update Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from 'antd';
+
+const Contact = () => (
+  <div id="contact">
+    <Typography.Title>Contact Me</Typography.Title>
+    <Typography.Paragraph>
+      You can reach me at email@example.com or follow me on social media.
+    </Typography.Paragraph>
+  </div>
+);
+
+export default Contact;
