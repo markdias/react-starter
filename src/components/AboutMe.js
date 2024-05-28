42566dc1d3e7e26252760cd406dc8a394a5ec04e
markdias <mark@mdias.co.uk>
2024-05-28 09:59:04 +0100

    Update AboutMe.js

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from 'antd';
+
+const AboutMe = () => (
+  <div id="about">
+    <Typography.Title>About Me</Typography.Title>
+    <Typography.Paragraph>
+      I am a passionate print designer with over 10 years of experience.
+    </Typography.Paragraph>
+  </div>
+);
+
+export default AboutMe;
