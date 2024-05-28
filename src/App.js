1922411cd4a791e6ef42a57a01ad952a9e767709
markdias <mark@mdias.co.uk>
2024-05-28 09:56:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
+import React from 'react';
+import { Layout } from 'antd';
+import { Element as ScrollElement } from 'react-scroll';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import './styles.css';
 
-function App() {
-  return (
-    // BEM naming convention
-    <div className="app">
-      <h1>React App</h1>
-    </div>
-  );
-}
+const { Header, Content, Footer } = Layout;
+
+const App = () => (
+  <Layout>
+    <Header>
+      <Navbar />
+    </Header>
+    <Content>
+      <ScrollElement name="home"><Home /></ScrollElement>
+      <ScrollElement name="about"><AboutMe /></ScrollElement>
+      <ScrollElement name="portfolio"><Portfolio /></ScrollElement>
+      <ScrollElement name="contact"><Contact /></ScrollElement>
+    </Content>
+    <Footer>
+      Social Media Links Here
+    </Footer>
+  </Layout>
+);
 
 export default App;
